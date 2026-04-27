41023bc014231da10a698a75063e0e50ae7e0ad7
shadaan001 <shadaan001@gmail.com>
2026-04-28 04:09:08 +0530

    aaaaaa

---
+++ app/admin/teachers/page.tsx
@@ -0,0 +1,11 @@
+
+"use client"
+
+export function TeachersPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-2xl font-bold">Teachers Page</h1>
+      <p>Manage teachers here</p>
+    </div>
+  )
+}
