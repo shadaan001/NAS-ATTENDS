1c52c327734b76d6f9f8d4631aaf34dddad8e4ab
shadaan001 <shadaan001@gmail.com>
2026-04-28 16:32:06 +0530

    add teachers page

--- app/admin/teachers/page.tsx
+++ app/admin/teachers/page.tsx
@@ -1,11 +1,5 @@
+import { TeachersPage } from "@/components/dashboard/teachers"
 
-"use client"
-
-export function TeachersPage() {
-  return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold">Teachers Page</h1>
-      <p>Manage teachers here</p>
-    </div>
-  )
+export default function Page() {
+  return <TeachersPage />
 }
